fb774e4b6ae66e059ebfa621572cccb619ef846d
mroziuk <mikolaj.mroziuk@gmail.com>
2022-02-02 22:02:40 +0100

    Add project files.

---
+++ Models/Address.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Transport.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public int Number { get; set; }
+    }
+}
---
+++ Models/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Transport.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public List<Product> Products { get; set; }
+        public Address DeliveryAddress { get; set; }
+        public string PaymentStatus { get; set; }
+        public string DeliveryStatus { get; set; }
+        public DateTime CreateTime { get; set; }
+    }
+}
